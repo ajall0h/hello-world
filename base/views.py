46eac64e04b4ed0dd2227a6d18dd6cc3adc1670f
[ajall0h] <[jallohaaron@gmail.com]>
2025-02-15 17:38:58 -0500

    2:32:25

---
+++ base/views.py
@@ -0,0 +1,36 @@
+from django. shortcuts import render, redirect 
+from django.db.models import Q 
+from django.contrib.auth.models import User 
+from models import Room, Topic 
+from .forms import RoomForm
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def home(request):
+    return render(request, 'room.html')
+# Create your views here.
+
+
+def loginpage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+            except: 
+
+                context = {}
+                return render(request, 'base/login_register.html', context)
+
+                def home(request):
+                    q = request.GET.get('q') if request.GET.get('q') != None else 
+
+                    rooms = Room.objects.filter(
+                        Q(topic__name__icontains=q) |
+                        Q (name__icontains=q) |
+                        Q(description__icontains=q)
+                    )
