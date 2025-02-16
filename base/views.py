7917ce5f0f580d0f2260ca6463522dd80ca020b9
[ajall0h] <[jallohaaron@gmail.com]>
2025-02-16 12:42:32 -0500

    3:27:30

--- base/views.py
+++ base/views.py
@@ -37,3 +37,15 @@ def loginpage(request):
                         Q (name__icontains=q) |
                         Q(description__icontains=q)
                     )
+
+@login_required(Login_url='login')
+def deleteMessage(request, pk):
+    message = message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+        return render(request, 'base/delete.html', {'obj' : message})
+        
